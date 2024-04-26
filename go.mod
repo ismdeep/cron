ae9a66b4a6cb2affca5df82f114fbe97b364578f
L. Jiang <l.jiang.1024@gmail.com>
2024-04-26 18:49:42 +0800

    feat: support multi cron spec

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/ismdeep/cron
 
 go 1.20
 
-require github.com/robfig/cron/v3 v3.0.1
+require (
+	github.com/ismdeep/log v0.2.2
+	github.com/robfig/cron/v3 v3.0.1
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	_ "embed"
 	"fmt"
 	"os"
@@ -8,17 +9,41 @@ import (
 	"strings"
 	"time"
 
+	"github.com/ismdeep/log"
 	"github.com/robfig/cron/v3"
+	"go.uber.org/zap"
 )
 
 //go:embed banner.txt
 var banner string
 
+var cronCommand string
+
+func runCommandFunc() {
+	startedAt := time.Now()
+	log.WithContext(context.Background()).Info("Started")
+	defer func() {
+		endedAt := time.Now()
+		log.WithContext(context.Background()).Info("Ended", zap.String("time_elapsed", endedAt.Sub(startedAt).String()))
+		log.WithContext(context.Background()).Info("----------------------------------------------------------------------")
+		fmt.Println()
+	}()
+	cmdSlice := strings.Fields(cronCommand)
+	cmd := exec.Command(cmdSlice[0], cmdSlice[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.WithContext(context.Background()).Error("failed to run task", zap.Error(err))
+		return
+	}
+	log.WithContext(context.Background()).Info("Finished")
+}
+
 func main() {
 	fmt.Println(banner)
 
 	cronSpec := os.Getenv("CRON_SPEC")
-	cronCommand := os.Getenv("CRON_COMMAND")
+	cronCommand = os.Getenv("CRON_COMMAND")
 	cronRunAtStart := os.Getenv("CRON_RUN_AT_START")
 
 	if cronSpec == "" {
@@ -29,29 +54,7 @@ func main() {
 		panic("CRON_COMMAND is empty")
 	}
 
-	runCommandFunc := func() {
-		fmt.Println("----------------------------------------------------------------------")
-		startedAt := time.Now()
-		fmt.Println("Started At:", startedAt.Format(time.RFC1123Z))
-
-		defer func() {
-			endedAt := time.Now()
-			fmt.Println("Ended At:", endedAt.Format(time.RFC1123Z))
-			fmt.Println("Time Elapse:", fmt.Sprintf("%v s", endedAt.Unix()-startedAt.Unix()))
-			fmt.Println("----------------------------------------------------------------------")
-			fmt.Println()
-		}()
-
-		cmdSlice := strings.Fields(cronCommand)
-		cmd := exec.Command(cmdSlice[0], cmdSlice[1:]...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			fmt.Println("Error:", err.Error())
-			return
-		}
-		fmt.Println("OK")
-	}
+	log.WithContext(context.Background()).Info("----------------------------------------------------------------------")
 
 	if cronRunAtStart == "true" {
 		runCommandFunc()
@@ -63,7 +66,16 @@ func main() {
 			cron.SkipIfStillRunning(
 				cron.DefaultLogger)),
 	)
-	_, _ = c.AddFunc(cronSpec, runCommandFunc)
+
+	for _, spec := range strings.Split(cronSpec, "\n") {
+		spec = strings.TrimSpace(spec)
+		if spec != "" {
+			log.WithContext(context.Background()).Info("Cron spec", zap.String("spec", spec))
+			if _, err := c.AddFunc(spec, runCommandFunc); err != nil {
+				panic(fmt.Errorf("failed to add cron spec '%s': %v", spec, err))
+			}
+		}
+	}
 
 	c.Run()
 }
